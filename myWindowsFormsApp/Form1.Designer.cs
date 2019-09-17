5812f6e25cfbcf98bf829afe2878fdadec84ee66
spark01 <btapas28@gmail.com>
2019-09-17 21:00:59 +0600

    day4

---
+++ myWindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace myWindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.showRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.ageLable = new System.Windows.Forms.Label();
+            this.ageTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(65, 76);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(35, 13);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Name";
+            // 
+            // showRichTextBox
+            // 
+            this.showRichTextBox.Location = new System.Drawing.Point(390, 47);
+            this.showRichTextBox.Name = "showRichTextBox";
+            this.showRichTextBox.Size = new System.Drawing.Size(257, 246);
+            this.showRichTextBox.TabIndex = 2;
+            this.showRichTextBox.Text = "";
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(206, 222);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(110, 36);
+            this.addButton.TabIndex = 3;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click_1);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(178, 76);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(138, 20);
+            this.nameTextBox.TabIndex = 4;
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(68, 226);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(85, 28);
+            this.showButton.TabIndex = 5;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // ageLable
+            // 
+            this.ageLable.AutoSize = true;
+            this.ageLable.Location = new System.Drawing.Point(74, 133);
+            this.ageLable.Name = "ageLable";
+            this.ageLable.Size = new System.Drawing.Size(26, 13);
+            this.ageLable.TabIndex = 6;
+            this.ageLable.Text = "Age";
+            // 
+            // ageTextBox
+            // 
+            this.ageTextBox.Location = new System.Drawing.Point(178, 126);
+            this.ageTextBox.Name = "ageTextBox";
+            this.ageTextBox.Size = new System.Drawing.Size(138, 20);
+            this.ageTextBox.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ageTextBox);
+            this.Controls.Add(this.ageLable);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.showRichTextBox);
+            this.Controls.Add(this.nameLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.RichTextBox showRichTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Label ageLable;
+        private System.Windows.Forms.TextBox ageTextBox;
+    }
+}
+
---
+++ myWindowsFormsApp/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myWindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        List<string> names = new List<string> {};
+        List<int> ages = new List<int> { };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void addButton_Click_1(object sender, EventArgs e)
+        {
+
+            // List<string> name = new List<string> { };
+            names.Add(nameTextBox.Text);
+            ages.Add(Convert.ToInt32(ageTextBox.Text));
+            
+            ShowCustomer();
+            //   string message = "";
+            // for ( int i= 0;i < names.count(); i++){
+            //      message += names[i]+ "\n";
+            //   }
+            // foreach(string name in names)
+            // {
+            //      message += "Name: " + name + "\n";
+            //  }
+            //
+            // showRichTextBox.Text = message;
+            // 
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            ShowCustomer();
+        }
+        private void ShowCustomer()
+        {
+            // List<string> name = new List<string> { };
+            // names.Add(nameTextBox.Text);
+            string message = "";
+            for (int i = 0; i < names.Count(); i++)
+            {
+                message += names[i]+" " + ages[i] + "\n";
+            }
+
+            ////foreach (string name in names)
+
+            //   {
+            //       message += "Name: " + name + "\n";
+            //   }
+
+            showRichTextBox.Text = message;
+        }
+
+
+
+    }
+}
+
